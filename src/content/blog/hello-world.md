3e920daa6e4447ad7b585ef2afd5fddb72316488
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2023-07-06 22:51:02 +1000

    Get src/content up and running

---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,9 @@
+---
+title: First
+tags: []
+publishDate: 2023-07-06
+relatedPosts: []
+---
+
+Moving my site from Hugo to Astro because who doesn't love trying new technology
+and procrasinating actually doing any writing.
---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { z, defineCollection, reference } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    publishDate: z.date(),
+    relatedPosts: z.array(reference('blog')),
+  }),
+});
+
+export const collections = { blog };
