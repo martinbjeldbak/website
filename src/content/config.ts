711ec1c64003b39a60f2e8ed4cc04334197e17f4
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2023-07-10 22:26:08 +1000

    Added tailwind and setup xo

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
-import { z, defineCollection, reference } from 'astro:content';
+import {z, defineCollection, reference} from 'astro:content';
 
 const blog = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    tags: z.array(z.string()),
-    publishDate: z.date(),
-    relatedPosts: z.array(reference('blog')),
-  }),
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		tags: z.array(z.string()),
+		publishDate: z.date(),
+		relatedPosts: z.array(reference('blog')),
+	}),
 });
 
-export const collections = { blog };
+export const collections = {blog};
