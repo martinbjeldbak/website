37d9583ff2a45c7f95452dadb595361bb0da69b9
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2023-07-25 22:08:00 +1000

    Add RSS link and styling

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,10 @@
 import rss from '@astrojs/rss';
 import {getCollection} from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
+
 
 export async function get(context) {
   const blog = await getCollection('blog');
@@ -14,6 +19,8 @@ export async function get(context) {
       description: post.data.description,
       customData: post.data.customData,
       link: `/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body)),
     })),
+    stylesheet: '/rss/pretty-feed-v3.xsl',
   });
 }
